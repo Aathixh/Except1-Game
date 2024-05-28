555eb321b5fccd1b2940c132822f9feafc673455
Aathish R Viswam <aathishrv@gmail.com>
2024-05-28 20:53:22 +0530

    initial conditions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const player0Score = document.querySelector('#score--0');
+const player1Score = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
+
+//Starting conditions of the game
+player0Score.textContent = 0;
+player1Score.textContent = 0;
+// document.querySelector('.dice').style.display = 'none';
+dice.classList.add('hidden');
