eb0d34320baf42b26cf98b2e31fbe9c233fee01a
Aathish R Viswam <aathishrv@gmail.com>
2024-06-06 19:28:14 +0530

    Add player element selection and active player toggle

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
-
+//Selecting elements
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 const player0Score = document.querySelector('#score--0');
 const player1Score = document.querySelector('#score--1');
 const player0CurrentScore = document.getElementById('current--0');
@@ -12,7 +14,9 @@ const btnHold = document.querySelector('.btn--hold');
 //Starting conditions of the game
 player0Score.textContent = 0;
 player1Score.textContent = 0;
+const totalScore = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 // document.querySelector('.dice').style.display = 'none';
 diceElement.classList.add('hidden');
 
@@ -28,8 +32,14 @@ btnDice.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    player0CurrentScore.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
